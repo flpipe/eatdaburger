433dcc1e051529b4a130db30f1bf6a8a9f434533
Revathy Venkataraman <revathyvenka@gmail.com>
2020-11-22 17:24:24 -0500

    uploading my solution so far

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bean Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+
