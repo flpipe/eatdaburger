4bb54d540317abc8a85c4321ba087e913f1db2c2
Revathy Venkataraman <revathyvenka@gmail.com>
2020-11-22 18:23:53 -0500

    uploading to jawsdbinfo

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static("/public"));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -27,3 +27,4 @@ app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
+
